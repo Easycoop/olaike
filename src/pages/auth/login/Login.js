da854410279cdbcfeae48f03d3bdd0b3f6484c0d
harry <johnharry238@gmail.com>
2024-09-04 13:54:33 +0100

    feat(setup): architecture

---
+++ src/pages/auth/login/Login.js
@@ -0,0 +1,14 @@
+import "./login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <section className="login__main">
+        <div className="login__main__start"></div>
+        <div className="login__main__end"></div>
+      </section>
+    </div>
+  );
+}
+
+export default Login;
